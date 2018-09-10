65b6cf77118377a7704c23bf0914bab24ab08363
David Blood <davidbjourno@users.noreply.github.com>
2018-09-10 11:37:20 +0100

    Refactor to include functions that update uniform values

--- index.js
+++ index.js
@@ -4,29 +4,54 @@ import helloWorldFrag from './01-hello-world.glsl'
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+const uniforms = {
+  u_time: { type: 'f', value: 1.0 },
+  u_resolution: { type: 'v2', value: new THREE.Vector2() },
+  u_mouse: { type: 'v2', value: new THREE.Vector2() }
+};
 
-const geometry = new THREE.PlaneBufferGeometry(2, 2);
-const material = new THREE.ShaderMaterial({
-  uniforms: {
-    u_time: { type: 'f', value: 1.0 },
-    u_resolution: { type: 'v2', value: new THREE.Vector2() },
-    u_mouse: { type: 'v2', value: new THREE.Vector2() }
-  },
-  vertexShader: vert,
-  fragmentShader: helloWorldFrag // Switch fragment shader here
-});
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+function onWindowResize(event) {
+  renderer.setSize(window.innerWidth, window.innerHeight);
 
-camera.position.z = 1;
+  uniforms.u_resolution.value.x = renderer.domElement.width;
+  uniforms.u_resolution.value.y = renderer.domElement.height;
+}
+
+function init() {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  const geometry = new THREE.PlaneBufferGeometry(2, 2);
+  const material = new THREE.ShaderMaterial({
+    uniforms,
+    vertexShader: vert,
+    fragmentShader: helloWorldFrag // Switch fragment shader here
+  });
+  const mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
+  camera.position.z = 1;
+
+  onWindowResize();
+  window.addEventListener('resize', onWindowResize, false);
+
+  document.onmousemove = (e) => {
+    uniforms.u_mouse.value.x = e.pageX
+    uniforms.u_mouse.value.y = e.pageY
+  }
+}
+
+function render() {
+  uniforms.u_time.value += 0.05;
+  
+  renderer.render(scene, camera);
+}
 
 function animate() {
   requestAnimationFrame(animate);
-  renderer.render(scene, camera);
+  render();
 };
 
+init();
 animate();
