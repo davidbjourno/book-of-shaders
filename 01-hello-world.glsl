2ba35962cea009a8886de3d7f402ff8da1f5c199
David Blood <davidbjourno@users.noreply.github.com>
2018-09-10 15:50:40 +0100

    Tidier annotations

--- 01-hello-world.glsl
+++ 01-hello-world.glsl
@@ -1,12 +1,15 @@
 // Preprocessor macros
+// Only insert line if `GL_ES` is defined (mostly happens when code is compiled on mobile devices/
+// browsers)
 #ifdef GL_ES
-// Only insert line if `GL_ES` is defined (mostly happens when code is compiled on mobile devices/browsers)
-precision mediump float; // Specify precision (low/medium/high) of each variable that uses floating point
+// Specify precision (low/medium/high) of each variable that uses floating point
+precision mediump float;
 #endif
 
 // GLSL has a single `main` function, here returning a colour
 void main() {
   // Final pixel color is assigned to reserved global variable `gl_FragColor`
-  gl_FragColor = vec4(1.0, 0.0, 1.0, 1.0); // `vec4` is a GLSL **type**, i.e. four-dimensional vector of floating-point precision
-                                           // Four vector arguments correspond to red, green, blue and alpha channels (normalised values)
+  // `vec4` is a GLSL **type**, i.e. four-dimensional vector of floating-point precision
+  // Four vector arguments correspond to red, green, blue and alpha channels (normalised values)
+  gl_FragColor = vec4(1.0, 0.0, 1.0, 1.0);
 }
--- 02-uniforms.glsl
+++ 02-uniforms.glsl
@@ -8,6 +8,7 @@ uniform vec2 u_mouse; // Mouse position in pixels
 uniform float u_time; // Time in seconds since load
 
 void main() {
-  gl_FragColor = vec4(abs(sin(u_time)), 0.0, 0.0, 1.0); // Use `u_time` with a sine function to continually modify the shader colour
-                                                        // GPU has hardware-accelerated angle, trigonometric and exponential functions
+  // Use `u_time` with a sine function to continually modify the shader colour
+  // GPU has hardware-accelerated angle, trigonometric and exponential functions
+  gl_FragColor = vec4(abs(sin(u_time)), 0.0, 0.0, 1.0);
 }
